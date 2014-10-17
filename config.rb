f38b6e4858f71a577e9a344d20eb5473c38c20f8
Matt Sullivan <msullivanxi@gmail.com>
2014-10-16 22:16:56 -0400

    v1
    ready for showtime

--- config.rb
+++ config.rb
@@ -1,52 +1,10 @@
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+activate :livereload
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+# Makes URL's pretty
+activate :directory_indexes
 
+# Sets Directories
 set :css_dir, 'stylesheets'
 
 set :js_dir, 'javascripts'
@@ -56,17 +14,23 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
-  # Enable cache buster
+  # Minify HTML
+  activate :minify_html
+
+  # # Enable cache buster
   # activate :asset_hash
 
-  # Use relative URLs
+  # # Use relative URLs
   # activate :relative_assets
 
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  # Makes URL's pretty
+  activate :directory_indexes
+
+  # # Or use a different image path
+  # set :http_prefix, "/images/"
 end
---
+++ source/javascripts/main.js
@@ -0,0 +1,6 @@
+//= require 'scripts/_jquery.js.erb'
+
+//= require 'scripts/_google-analytics.js.erb'
+
+//= require 'scripts/_smooth-scroll.js.erb'
+
