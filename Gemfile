7e78740b9a461aa70a51b9590f52ec8af246ab95
Matt Sullivan <matt.sullivan@voxmedia.com>
2016-03-06 17:23:53 -0500

    website v2

--- Gemfile
+++ Gemfile
@@ -1,17 +1,14 @@
-# If you have OpenSSL installed, we recommend updating
-# the following line to use "https"
-source 'http://rubygems.org'
-
-gem "middleman", "~>3.3.6"
-
-# Live-reloading plugin
-gem "middleman-livereload", "~> 3.1.0"
+# If you do not have OpenSSL installed, change
+# the following line to use 'http://'
+source 'https://rubygems.org'
 
 # For faster file watcher updates on Windows:
-gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
+gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
 
 # Windows does not come with time zone data
-gem "tzinfo-data", platforms: [:mswin, :mingw]
+gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 
-# image optimizer
-gem 'middleman-imageoptim'
+# Middleman Gems
+gem 'middleman', '>= 4.0.0'
+gem 'middleman-livereload'
+gem 'middleman-autoprefixer'
--- config.rb
+++ config.rb
@@ -1,42 +1,50 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+# General configuration
+
 # Reload the browser automatically whenever files change
-activate :livereload
+configure :development do
+  activate :livereload
+  activate :autoprefixer
+end
 
-# Makes URL's pretty
-activate :directory_indexes
+###
+# Helpers
+###
 
-# Sets Directories
-set :css_dir, 'stylesheets'
-
-set :js_dir, 'javascripts'
-
-set :images_dir, 'images'
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
+
+  # Autoprefixed CSS
+  activate :autoprefixer
+
+  # Minify CSS on build
   activate :minify_css
 
   # Minify Javascript on build
   activate :minify_javascript
 
-  # Minify HTML
-  activate :minify_html
-
-  # gzips things for faster loading
-  activate :gzip
-
-  # optimizes images to reduce load times
-  activate :imageoptim
-
-  # # Enable cache buster
-  # activate :asset_hash
-
-  # # Use relative URLs
-  # activate :relative_assets
-
-  # Makes URL's pretty
-  activate :directory_indexes
-
-  # # Or use a different image path
-  # set :http_prefix, "/images/"
 end
